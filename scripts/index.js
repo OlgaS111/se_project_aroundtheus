58b1fb1571410fbd8590c0b10cda35dffab79e22
OlgaKlochan <olgaklochan1@gmail.com>
2024-10-15 17:41:09 -0500

    -

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ const cardListEl = document.querySelector('.cards__list');
 const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
 
 function closePopup() {
-    profileEditModal.classList.remove('modal_opened');
+    profileEditModal.classList.remove('modal__opened');
 }
 
 function getCardElement(cardData) {
